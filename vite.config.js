788ec6e16eb366cc991d810be7e89c8628c087d0
Makc240305 <maxim240305@gmail.com>
2025-01-28 13:07:09 +0100

    Add new base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'https://github.com/Makc240305/Counter-App.git',
+  base:'/Counter-App/',
   plugins: [react()],
 })
